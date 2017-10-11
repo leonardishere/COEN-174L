a4ba95ad097de6e0d64ba763d774fb500f7375a9
Andrew Leonard <leonardishere@gmail.com>
2017-10-11 11:42:18 -0700

    made tables sortable

--- api/routes/equiv_course.ts
+++ api/routes/equiv_course.ts
@@ -1,6 +1,8 @@
 import { Database } from '../database';
 import * as PromiseRouter from 'express-promise-router';
 
+var fs = require('fs');
+
 var db = new Database();
 var router = PromiseRouter();
 router.route('/')
@@ -37,15 +39,19 @@ router.route('/:SCUClassID')
   });
   
 function sendResults(res, result){
+	var tableID = "justAnotherTable";
 	var columnNames = ["SCU Course", "Foreign Course", "School", "Status"];
 	var columns = ["LocalCourseName", "ForeignCourseName", "SchoolName", "Status"];
 	
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	
-	res.write("<table>");
+	res.write("<p>table is sortable if you click on the header name</p>");
+	res.write("<table id=\""+tableID+"\">");
 	res.write("<tr>");
+	var i = 0;
 	columnNames.forEach((entry) => {
-		res.write("<th>"+entry+"</th>");
+		res.write("<th onclick=\"sortTable('"+tableID+"',"+i+")\">"+entry+"</th>");
+		++i;
 	});
 	res.write("</tr>");
 	result.forEach((row) => {
@@ -56,6 +62,13 @@ function sendResults(res, result){
 		res.write("</tr>");
 	});
 	res.write("</table>");
+	
+	res.write("<script>");
+	var filename = "tableSortScript.js";
+	var data = fs.readFileSync(filename, "utf8");
+	res.write(data);
+	res.write("</script>");
+	
 	return res.end();
 }
 
