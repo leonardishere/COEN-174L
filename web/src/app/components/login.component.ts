ac12f6979fc878ec136b02eae849a519dc179569
Rowan Decker <Smasher816@gmail.com>
2017-11-14 14:48:58 -0800

    Create quick login screen

--- web/src/app/components/login.component.ts
+++ web/src/app/components/login.component.ts
@@ -3,12 +3,21 @@ import { environment } from '../../environments/environment'
 
 @Component({
   selector: 'redirect',
-  template: 'redirecting...'
+	template: `<div class="container">
+	<div class="row justify-content-center" style="margin-bottom: 20px;">
+	<img width="256" height="256" src="http://books-not-bombs.com/content/images/schools/scu.png" />
+	</div>
+	<div class="row justify-content-center">
+	<a class="btn btn-info" href="{{redirectUrl}}">Login</a>
+	</div>
+  `
 })
 export class LoginComponent implements OnInit {
+  redirectUrl = environment.api + 'auth';
+
   constructor() { }
 
   ngOnInit() {
-    window.location.href = environment.api + 'auth';
+	  //window.location.href = this.redirectUrl;
   }
 }
